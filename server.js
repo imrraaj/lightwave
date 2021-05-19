7d1b4cd7df52c9cecf526d4c4f802e346a4a46b6
imrraaj <71687258+imrraaj@users.noreply.github.com>
2021-05-19 17:48:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 80;
+
+let user = {};
+
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('Index');
+})
+
+app.post('/query', (req, res) => {
+    // res.redirect('https://raajwrites.wordpress.com')
+    res.redirect('/');
+    let Name = req.body.name
+    let Email = req.body.email
+    let Query = req.body.query
+    console.log("Name : "+ Name + "\nEmail : " + Email + "\nQuery : " + Query+"\n\n");
+})
+
+app.listen(PORT, () => {
+    console.log('running at https://127.0.0.1:80');
+})
